2d9ed2b7ccd6e5b724b12bb213ce91ec7b1c45e1
josileudo <josileudorodrigues75@gmail.com>
2023-08-01 12:07:26 -0300

    :hammer: chore: Config linters rules

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      declarations: [AppComponent]
+    })
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -22,6 +25,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.wrapper')?.textContent).toContain(`Welcome to create-mfe-app`);
+    expect(compiled.querySelector('.wrapper')?.textContent).toContain(
+      `Welcome to create-mfe-app`
+    );
   });
 });
